12d0c910f9c7643a41c77dda63e9c4ea78517a67
dengxiang <angried@163.com>
2019-05-28 17:38:26 +0800

    first commit

---
+++ src/pages/user/add.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<style lang="less" scoped>
+</style>
+
+<script>
+import Vue from "vue";
+
+export default {
+  data() {
+    return {
+      msg: "This is Vug"
+    };
+	},
+	created(){
+		console.log(this)
+	}
+};
+</script>
