58ab2516182334db865e1aa22ba44b5ddafe4073
dengxiang <angried@163.com>
2019-05-29 15:07:10 +0800

    update

--- src/pages/user/add.vue
+++ src/pages/user/add.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <h3 v-show="show">Welcome</h3>
     <p>{{msg}}</p>
+    <a href="#" @click="showHi">click</a>
   </div>
 </template>
 
@@ -9,15 +11,28 @@
 
 <script>
 import Vue from "vue";
+import { Toast } from "mint-ui";
+import "mint-ui/lib/style.css";
 
 export default {
   data() {
     return {
-      msg: "This is Vug"
+      msg: "This is Vug",
+      show: true
     };
-	},
-	created(){
-		console.log(this)
-	}
+  },
+  created() {
+    console.log(this);
+  },
+  methods: {
+    showHi() {
+      // this.show = !this.show;
+      Toast({
+        message: "请登录",
+        duration: 3000,
+        position: "bottom"
+      });
+    }
+  }
 };
 </script>
